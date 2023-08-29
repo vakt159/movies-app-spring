4eb47155a7750bdb514f5ef7cc02db2b13a5b16a
Andriy <vakt159@gmail.com>
2023-08-29 19:39:57 +0300

    Test1

--- src/main/java/com/moviesdb/moviesdb/persistence/ActorDAO.java
+++ src/main/java/com/moviesdb/moviesdb/persistence/ActorDAO.java
@@ -2,8 +2,7 @@ package com.moviesdb.moviesdb.persistence;
 
 import com.moviesdb.moviesdb.models.Actor;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface ActorDAO extends JpaRepository<Actor,Long> {
-    Actor findActorByFirstNameAndLastName(String firstName,String lastName);
+     Actor findActorByFirstNameAndLastName(String firstName,String lastName);
 }
