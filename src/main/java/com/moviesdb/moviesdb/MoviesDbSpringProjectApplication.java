d590d5d3fcf095a90fae6d5564d338cdfa1f093d
Andriy <vakt159@gmail.com>
2023-08-29 10:19:59 +0300

    project created

---
+++ src/main/java/com/moviesdb/moviesdb/MoviesDbSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.moviesdb.moviesdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesDbSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesDbSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moviesdb/moviesdb/MoviesDbSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviesdb.moviesdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesDbSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
