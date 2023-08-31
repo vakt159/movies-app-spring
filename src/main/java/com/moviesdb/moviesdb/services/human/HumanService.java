9e8f155104c72e6f9cac1681da912721b90a5eb1
ArtomDovgal <artomdovgalnew@gmail.com>
2023-08-31 10:18:02 +0300

    add director's controller #2

--- src/main/java/com/moviesdb/moviesdb/services/human/HumanService.java
+++ src/main/java/com/moviesdb/moviesdb/services/human/HumanService.java
@@ -11,4 +11,12 @@ public interface HumanService<T extends HumanBaseEntity> {
     T save(T human);
     void deleteById(Long id);
     T update(T human, Long id);
+
+    void deleteTVShow(Long humanId, Long tvShowId);
+
+    void deleteMovie(Long humanId, Long movieId);
+
+    void addTVShow(Long tvShowId, Long humanId);
+
+    void addMovie(Long movieId, Long humanId);
 }
